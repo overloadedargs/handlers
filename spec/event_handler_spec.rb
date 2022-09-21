4c63189ba9802f854b1f26359d816b7114b3cd03
Crawford Wynnes <crawfordwynnes@gmail.com>
2022-09-21 15:04:51 +0100

    Calls broadcast

--- spec/event_handler_spec.rb
+++ spec/event_handler_spec.rb
@@ -1,27 +1,30 @@
 require 'spec_helper'
 
+# Maintaining id list?
+#
 describe EventHandler do
     let(:event_handler) { EventHandler.new }
-    let(:handler) { |x| "Handler" }
+
+    before do
+      @handler = ->(args) { p args; }
+    end
 
     describe '#subscribe' do
       it 'can receive the new block' do
         expect(event_handler).to receive(:subscribe) do |&block|
-          expect(block.call).to eql("Handler")
           expect(block).to be_a(Proc) 
         end
-        event_handler.subscribe { handler }
+        event_handler.subscribe { @handler }
       end
 
       it 'stores the block' do
-        event_handler.subscribe { handler }
+        event_handler.subscribe { @handler }
         expect(event_handler.handlers.length).to eq(1)
       end
     end
 
     describe '#unsubscribe' do
       it 'removes the block' do
-        @handler = handler
         event_handler.subscribe { @handler }
         h = event_handler.subscribe { @handler }
         event_handler.unsubscribe { h }
@@ -30,6 +33,11 @@ describe EventHandler do
     end
 
     describe '#broadcast' do
+      it 'can call all the blocks with arguments' do
+        event_handler.subscribe { @handler }
+        event_handler.subscribe { @handler }
+        expect(event_handler.broadcast(1,2,3)).to eq(true)
+      end
     end
 
 end
--- src/EventHandler.rb
+++ src/EventHandler.rb
@@ -7,19 +7,30 @@ class EventHandler
       @handlers = []
     end
 
+    # Block passed is another block that is to be called by all subscribers
+    #
     def subscribe(&block)
       h = Hash.new
       key = SecureRandom.uuid.to_sym
-      h[key] = block
+      h[key] = block.call
       @handlers << h
-      return key
+      key
     end
 
-    # This block is just an id to the required unsubscribed id
+    # This block is just an id to the required unsubscribed block id
     #
     def unsubscribe(&block)
       id = block.call
-      l = @handlers.select { |h| p h.first; h.first.first == id }
+      l = @handlers.select { |h| h.first.first == id }
       @handlers.delete(l.first)
+    end
+
+    def broadcast(*args)
+      @handlers.each do |h|
+        h.each do |_,v|
+          v.call(args)
+        end
+      end
+      true
     end
 end
